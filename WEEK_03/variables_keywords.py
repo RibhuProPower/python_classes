8e5edc7133a1200f9845034068dc45ee11b0085c
RibhuProPower <Ribhugovardhanagiri@gmail.com>
2019-01-13 15:58:47 +0530

    first commit

---
+++ WEEK_03/variables_keywords.py
@@ -0,0 +1,9 @@
+f = float(input("Enter the number"))
+a = f-int(f)
+if a>=0.5 :
+    print("The approximation of "+str(f)+ " is "+str(int(f)+1))
+
+else:
+    print("The approximation of "+str(f)+ " is "+str(int(f)))
+
+
