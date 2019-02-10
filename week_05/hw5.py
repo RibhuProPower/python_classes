096f0162a93c09bc3f5a9abafa273dd999f5362b
RibhuProPower <Ribhugovardhanagiri@gmail.com>
2019-02-10 15:51:57 +0530

    week 7

--- week_05/hw5.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Enter any POSITIVE INTEGER."))
-lpvr = 1
-fact = 1
-while (lpvr <= n):
-    fact = fact * lpvr
-    lpvr += 1
-print (str(n) + " factorial is " + str(fact))
--- week_06/string.py
+++ week_06/string.py
@@ -1,4 +1,4 @@
 um = str(input("EnTeR SoMe tExT."))
 for qwerty in um:
     print ("current letter:" + qwerty)
-print(str(len(um)) + " is the number of charecters. wuhdcbnu")
+print(str(len(um)) + ' is the number of charecters. wuhdcbnu')
